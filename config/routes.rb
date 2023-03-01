18e3138caa8bc26c701cf94befef7c3b20738037
Federico Luna <fluna1973@yahoo.com>
2023-02-28 21:47:11 -0600

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "users#index"
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create] do 
